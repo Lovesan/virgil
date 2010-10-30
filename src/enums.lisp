cbe6cfb0889ee8a35e50601e42c2834219f9fb9b
Dmitriy Ignatiev <lovesan.ru@gmail.com>
2010-10-30 17:54:38 +0400

    Strongly typed enums

--- src/enums.lisp
+++ src/enums.lisp
@@ -35,40 +35,86 @@
                  :reader enum-type-vk)
    (base-type :initarg :base-type
               :initform (parse-typespec 'int)
-              :reader base-type))
+              :reader base-type)
+   (list :initarg :list
+         :initform nil
+         :reader enum-type-list-p))
   (:prototype (type) 0)
   (:prototype-expansion (type) 0)
   (:lisp-type (type)
     `(or (member ,@(mapcar #'first (enum-type-kv type)))
-         ,(lisp-type (base-type type))))
+         ,(if (enum-type-list-p type)
+            'list
+            (lisp-type (base-type type)))))
   (:converter (lisp-value type)
-    (etypecase lisp-value
-      (integer lisp-value)
-      (keyword (or (second (assoc lisp-value (enum-type-kv type)))
-                   (error "~s is invalid keyword for enum type ~s"
-                          lisp-value (unparse-type type))))))
+    (if (enum-type-list-p type)
+      (loop :with result = 0
+        :with kv = (enum-type-kv type)
+        :for key :in (ensure-list lisp-value)
+        :do (setf result (logior result
+                                 (or (second (assoc key kv))
+                                     (error "~s is invalid keyword for enum type ~s"
+                                            lisp-value (unparse-type type)))))
+        :finally (return result))
+      (etypecase lisp-value
+        (integer lisp-value)
+        (keyword (or (second (assoc lisp-value (enum-type-kv type)))
+                     (error "~s is invalid keyword for enum type ~s"
+                            lisp-value (unparse-type type)))))))
   (:translator (raw-value type)
-    (or (second (assoc raw-value (enum-type-vk type) :test #'=))
-        raw-value))
+    (if (enum-type-list-p type)
+      (loop :with list = '()
+        :for vk :in (enum-type-vk type)
+        :when (or (zerop (car vk))
+                  (/= 0 (logand raw-value (car vk))))
+        :do (push (second vk) list)
+        :finally (return list))
+      (or (second (assoc raw-value (enum-type-vk type) :test #'=))
+          raw-value)))
   (:converter-expansion (lisp-value-form type)
-    (with-gensyms (lisp-value)
-      `(let ((,lisp-value ,lisp-value-form))
-         (declare (type ,(lisp-type type) ,lisp-value))
-         (etypecase ,lisp-value
-           (integer ,lisp-value)
-           (keyword (case ,lisp-value
-                      ,@(enum-type-kv type)
-                      (T (error "~s is invalid keyword for enum type ~s"
-                                ,lisp-value
-                                ',(unparse-type type)))))))))
+    (if (constantp lisp-value-form)
+      (convert-value (eval lisp-value-form) type)
+      (with-gensyms (lisp-value result key)
+        `(let ((,lisp-value ,lisp-value-form))
+           (declare (type ,(lisp-type type) ,lisp-value))
+           ,(if (enum-type-list-p type)
+              `(let ((,result 0))
+                 (declare (type ,(lisp-type (base-type type)) ,result))
+                 (loop :for ,key :of-type keyword :in (ensure-list ,lisp-value)
+                   :do (setf ,result
+                             (logior ,result
+                                     (ecase ,key
+                                       ,@(enum-type-kv type)
+                                       (T (error "~s is invalid keyword for enum type ~s"
+                                                 ,key
+                                                 ',(unparse-type type)))))))
+                 ,result)
+              `(etypecase ,lisp-value
+                 (integer ,lisp-value)
+                 (keyword (case ,lisp-value
+                            ,@(enum-type-kv type)
+                            (T (error "~s is invalid keyword for enum type ~s"
+                                      ,lisp-value
+                                      ',(unparse-type type)))))))))))
   (:translator-expansion (raw-value-form type)
-    (with-gensyms (raw-value)
-      `(let ((,raw-value ,raw-value-form))
-         (declare (type ,(lisp-type (base-type type))
-                        ,raw-value))
-         (case ,raw-value
-           ,@(enum-type-vk type)
-           (T ,raw-value)))))
+    (if (constantp raw-value-form)
+      (translate-value (eval raw-value-form) type)
+      (with-gensyms (raw-value list)
+        `(let ((,raw-value ,raw-value-form))
+           (declare (type ,(lisp-type (base-type type))
+                          ,raw-value))
+           ,(if (enum-type-list-p type)
+              `(let ((,list '()))
+                 (declare (type list ,list))
+                 ,(loop :for vk :in (enum-type-vk type)
+                    :collect (if (zerop (car vk))
+                               `(push ',(second vk) ,list)
+                               `(unless (zerop (logand ,raw-value ,(car vk)))
+                                  (push ',(second vk) ,list))))
+                 ,list)
+              `(case ,raw-value
+                 ,@(enum-type-vk type)
+                 (T ,raw-value)))))))
   (:cleaner (pointer value type) nil)
   (:cleaner-expansion (pointer value type) nil)
   (:allocator-expansion (value type)
@@ -113,15 +159,17 @@
 
 (define-type-parser enum (options &rest enum-list)
   (destructuring-bind
-    (&key (base-type 'int)) options
+    (&key (base-type 'int) list) options    
     (multiple-value-bind
         (kv vk) (parse-enum-list enum-list)
       (make-instance 'enum-type
         :kv kv :vk vk
+        :list list
         :base-type (parse-typespec base-type)))))
 
 (defmethod unparse-type ((type enum-type))
-  `(enum (:base-type ,(unparse-type (base-type type)))
+  `(enum (:base-type ,(unparse-type (base-type type))
+          :list ,(and (enum-type-list-p type) t))
          ,@(enum-type-kv type)))
 
 (defmethod unparse-type ((type named-enum-type))
@@ -138,7 +186,8 @@
         (name options))
     (destructuring-bind
         (&key (conc-name (intern (format nil "~a-" name)))
-              (base-type 'int))
+              (base-type 'int)
+              list)
         (flatten-options options)
       (when (null conc-name)
         (setf conc-name ""))
@@ -149,6 +198,7 @@
                  (make-instance 'named-enum-type
                    :base-type (parse-typespec ',base-type)
                    :name ',name
+                   :list ,(and list t)
                    :kv ',kv
                    :vk ',vk))
            (define-type-parser ,name ()
@@ -158,4 +208,6 @@
                            ,v))
            (deftype ,name () 
              '(or (member ,@(mapcar #'first kv))
-               ,(lisp-type (parse-typespec base-type)))))))))
+               ,(if list
+                  'list
+                  (lisp-type (parse-typespec base-type))))))))))
